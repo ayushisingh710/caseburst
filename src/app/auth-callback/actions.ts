8b469f8ebe59ecc6463660666792ed8b6a390dab
Ayuhi <ayushisingh73448@gmail.com>
2024-06-25 22:25:19 +0530

    first commit

---
+++ src/app/auth-callback/actions.ts
@@ -0,0 +1,29 @@
+"use server"
+
+import { db } from "@/db"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+
+export const getAuthStatus=async()=>{
+    const {getUser} = getKindeServerSession()
+
+    const user = await getUser()
+    if(!user?.id || !user?.email){
+        throw new Error('Invalid user data')
+
+    }
+
+    const exixstingUser = await db.user.findFirst({
+        where:{id:user.id},
+    })
+
+    if(!exixstingUser){
+        await db.user.create({
+            data:{
+                id:user.id,
+                email:user.email,
+            }
+        })
+    }
+
+    return{success:true}
+}
